72cc4b073f7a07e3428c405a4cc1aff0b6ef8247
Allerim <mirellatcg@gmail.com>
2024-02-29 14:54:21 -0300

    up

--- components/Bloco.tsx
+++ components/Bloco.tsx
@@ -1,22 +1,10 @@
-import { Feather, Octicons } from '@expo/vector-icons';
 import {View, StyleSheet} from "react-native";
 
 function Bloco() {
     return(
-    <View style={styles.icons}>
-    <Octicons name="three-bars" size={24} color="black" />
-    <Feather name="search" size={24} color="black" />
-    </View>
-)}
-
-const styles = StyleSheet.create({
-
-    icons: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        margin: 20,
-        borderBottomColor: "#F5F5F5"
+        <View></View>
+    )
     }
-})
+
 
 export default Bloco
--- screens/Inventarios.tsx
+++ screens/Inventarios.tsx
@@ -1,12 +1,28 @@
 import {Text, View, StyleSheet} from "react-native";
+import { Feather, Octicons } from '@expo/vector-icons';
 import Bloco from "../components/Bloco";
 
 function Inventario(){
     return(
         <View>
+            <View style={styles.icons}>
+            <Octicons name="three-bars" size={24} color="black" />
+            <Feather name="search" size={24} color="black" />
+            </View>
             <Bloco />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+
+    icons: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: 20,
+        borderColor: "#F5F5F5"
+    }
+})
+
 
 export default Inventario
